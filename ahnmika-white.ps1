95a17cb846efc2527a7d97fdf2a44d77775913e1
takayo 2020 <63852665+takayo2020@users.noreply.github.com>
2022-05-08 19:22:04 +0900

    Add files via upload

---
+++ ahnmika-white.ps1
@@ -0,0 +1,14 @@
+﻿try
+{
+    $count=0
+  while($true)
+  {
+  $colors = Get-Random -Minimum 100 -Maximum 1000
+  Write-Host ("白って${colors}色あんねん")
+  $count++
+  }
+}
+finally
+{
+  Write-Host "${count}人のアンミカ"
+}
